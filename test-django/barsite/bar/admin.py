c4daaa13dac54e2a635f322e83df5750dab55d65
giadagarrisi <giadina_13@hotmail.com>
2020-02-06 15:35:35 +0100

    Revert "aggiunta cocktails su db"
    This reverts commit 275f88dea7c76d93683f9f7582430db0d24298e3.

--- test-django/barsite/bar/admin.py
+++ test-django/barsite/bar/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-#from .models import Cocktail
+
 # Register your models here.
-#admin.site.register(Cocktail)
--- test-django/barsite/bar/views.py
+++ test-django/barsite/bar/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Persona
-from django.template import loader
 
 
 # Create your views here.
 
 def home(request):
     return HttpResponse('Hello, world. You are at the home page. Do you want to check the menu?')
-
