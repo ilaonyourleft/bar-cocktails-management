951675046a98f104c4a1ad4c32ceaa0963c7e080
ilaonyourleft <ilariamalinconico@gmail.com>
2020-02-08 15:50:48 +0100

    add Persona giada

--- test-django/barsite/barsite/settings.py
+++ test-django/barsite/barsite/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         # 'HOST': 'localhost',
         # 'PORT': '5432',
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db/db.sqlite3'),
     }
 }
 
