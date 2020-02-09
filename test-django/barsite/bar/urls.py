52124ceb3981f1dfa91990c3f18f4489c32199e0
Your Name <you@example.com>
2020-02-09 18:53:27 +0100

    aggiunto login switch per cliente e barista

--- test-django/barsite/bar/urls.py
+++ test-django/barsite/bar/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.HomepageView.as_view(), name='home'),
+    path('area-riservata/', views.login, name='login'),
     path('registrazione/', views.registrazione, name='registrazione'),
-    path('ordinazione/', views.login, name='login'),
+    # path('ordinazione/', views.login, name='login'),
     path('codice-prenotazione/', views.codicePrenotazione, name='codice-prenotazione'),
-    path('modifica-menu/', views.modificaMenu, name='modifica-menu'),
+    # path('modifica-menu/', views.login, name='login'),
     path('inserisci-cocktail/', views.inserisciCocktail, name='inserisci-cocktail'),
     path('controllo-codice/', views.controlloCodice, name='controllo-codice'),
 ]
--- test-django/barsite/bar/views.py
+++ test-django/barsite/bar/views.py
@@ -1,19 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from bar.models import Cocktail, Persona, Cliente, Barista
 
 
 # Create your views here.
-from django.views import generic
-from bar.models import Cocktail, Persona
 
-
-# def home(request):
-#     #return HttpResponse('Hello, world. You are at the home page. Do you want to check the menu?')
-#     cocktail_list = Cocktail.objects.all()
-#     context = {
-#             'cocktail_list': cocktail_list,
-#         }
-#     return render(request, 'bar/homepage.html', context)
 
 class HomepageView(generic.ListView):
     template_name = 'bar/homepage.html'
@@ -28,13 +20,38 @@ def login(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
 
-        p = Persona.objects.get(email=email, password=password)
+        try:
+            p = Persona.objects.get(email=email, password=password)
+        except Persona.DoesNotExist as not_p:
+            print(not_p)
+            return HttpResponse('Nessun utente registrato con queste credenziali.')
+        else:
+            p_id = p.id
 
-        context = {
-            'persona': p,
-        }
+            try:
+                type_c = Cliente.objects.get(id=p_id)
+            except Cliente.DoesNotExist as not_c:
+                print(not_c)
+                try:
+                    type_b = Barista.objects.get(id=p_id)
+                except Barista.DoesNotExist as not_b:
+                    print(not_b)
+                else:
+                    print(type_b)
+                    context = {
+                        'persona': p,
+                        'type': 'barista',
+                    }
 
-        return render(request, 'bar/ordinazione.html', context)
+                    return render(request, 'bar/area-riservata.html', context)
+            else:
+                print(type_c)
+                context = {
+                    'persona': p,
+                    'type': 'cliente',
+                }
+
+                return render(request, 'bar/area-riservata.html', context)
 
 
 def registrazione(request):
@@ -43,10 +60,6 @@ def registrazione(request):
 
 def codicePrenotazione(request):
     return HttpResponse('Pagina di codice prenotazione.')
-
-
-def modificaMenu(request):
-    return HttpResponse('Pagina di modifica menu.')
 
 
 def inserisciCocktail(request):
